98248bc7d4f0361baa508918df43855050cc497b
Pål Gjerde <paalvaa@gmail.com>
2016-11-18 20:25:50 +0100

    Problem 4

--- src/problem4/Main.java
+++ src/problem4/Main.java
@@ -8,42 +8,6 @@ import no.patternsolutions.javann.Kohonen;
 public class Main {
 
 	private static String letters = "ABCDEJK";
-
-	// Prints a list of letters suggested by an output set
-	// private static String findLetter(boolean[] output) {
-	// StringBuilder sb = new StringBuilder();
-	// boolean first = true;
-	// for (int i = 0; i < output.length; ++i) {
-	// if (output[i]) {
-	// if (!first) sb.append(", ");
-	// sb.append(letters.charAt(i));
-	// first = false;
-	// }
-	// }
-	// return sb.toString();
-	// }
-
-	// Prints a letter suggested by a double list
-	private static String findLetter(double[] output) {
-		StringBuilder sb = new StringBuilder();
-		boolean first = true;
-		for (int i = 0; i < output.length; ++i) {
-			if (output[i] >= 0.5) {
-				if (!first)
-					sb.append(", "); // Won't happen for outputs with total
-										// value <= 1
-				sb.append(letters.charAt(i));
-				first = false;
-			}
-		}
-		return sb.toString();
-	}
-	
-	private static String compare(double[] pattern, double[] kohonenPattern){
-		
-		
-		return null;
-	}
 	
 	public static double[][] createRandomCompLayer(int i, int j) {
 		double[][] d = new double[i][j];
@@ -61,15 +25,6 @@ public class Main {
 			for (int k = 0; k < arr[i].length; ++k) {
 				out[i][k] = arr[i][k] ? 1.0 : 0.0;
 			}
-		}
-		return out;
-	}
-
-	// Converts a boolean true/false array to a double 1.0/0.0 array.
-	private static double[] toDoubles(boolean[] arr) {
-		double[] out = new double[arr.length];
-		for (int i = 0; i < arr.length; ++i) {
-			out[i] = arr[i] ? 1 : 0;
 		}
 		return out;
 	}
@@ -95,9 +50,9 @@ public class Main {
 		kohonen.setLearnRate(0.5);
 		//koh.setNetworkSize(2, 2);
 		kohonen.setCompetingLayer(competingLayer);
-		kohonen.setDecreseLearnRate(true);
+		kohonen.setDecreaseLearnRate(true);
 		kohonen.setMatchType(Kohonen.EUCLIDIC_DISTANCE);
-		kohonen.setNeighbourDecrese(Kohonen.LINEAR_DECRESE);
+		kohonen.setNeighbourDecrease(Kohonen.LINEAR_DECRESE);
 		kohonen.setNeighbourRange(0.65);
 
 		kohonen.trainPatterns(dFont1);
@@ -113,10 +68,8 @@ public class Main {
 			System.out.println("Font " + (f + 1));
 			for (int i = 0; i < letters.length(); ++i) {
 				int out = kohonen.runIndex(fonts[f][i]);
-				double[] answer = kohonen.run(fonts[f][i]);
 				System.out.print("Testing with " + letters.charAt(i) + ": ");
-				//System.out.println(findLetter(out));
-				System.out.println(compare(fonts[f][i], answer));
+				System.out.println(out);
 				
 			}
 			System.out.println();
